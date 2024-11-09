0e84400465e0a5d55425cef4eae8b39205b275ad
sierrapv <sierra.xpplg2@gmail.com>
2024-11-09 08:30:27 +0700

    second commit

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -17,30 +17,33 @@ class MyHomePage extends StatelessWidget {
         foregroundColor: Colors.white,
       ),
       drawer: buildGroupDrawer(context),
-      body: GridView.count(
-        crossAxisCount: 2,
-        crossAxisSpacing: 10.0,
-        mainAxisSpacing: 10.0,
-        shrinkWrap: true ,
-        children: List.generate(10, (index){
-            return Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Container(
-                height: 5.0,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20.0),
-                  color: Colors.blue[200],
-                ),
-                child: Text('Materi $index',
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                ),
-                ),
-              ),
-            );
-          })
+      // body: GridView.count(
+      //   crossAxisCount: 2,
+      //   crossAxisSpacing: 10.0,
+      //   mainAxisSpacing: 10.0,
+      //   shrinkWrap: true ,
+      //   children: List.generate(10, (index){
+      //       return Padding(
+      //         padding: const EdgeInsets.all(10.0),
+      //         child: Container(
+      //           height: 5.0,
+      //           decoration: BoxDecoration(
+      //             borderRadius: BorderRadius.circular(20.0),
+      //             color: Colors.blue[200],
+      //           ),
+      //           child: Text('Materi $index',
+      //           textAlign: TextAlign.center,
+      //           style: const TextStyle(
+      //             color: Colors.black,
+      //             fontSize: 20,
+      //           ),
+      //           ),
+      //         ),
+      //       );
+      //     })
+      // ),
+      body: Center(
+        child: Text('Pemrograman Mobile $title'),
       ),
     );
   }
@@ -52,61 +55,86 @@ class MyHomePage extends StatelessWidget {
         children: [
           DrawerHeader(
             decoration: BoxDecoration(color: Colors.blue[900]),
-            child: const Text ('Sierra Putri Valenci',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 30.0,
+            child: UserAccountsDrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue[900]),
+              accountName: Text('Sierra Putri Valenci',
+              style: TextStyle(
+                fontSize: 18,
               ),
-            ),
+              ),
+              accountEmail: Text('sierra.xpplg2@gmail.com'),
+              currentAccountPictureSize: Size.square(40),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Text('S',
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.blue,
+                ),
+                ),
+              ),
+              ),
+            // child: const Text ('Sierra Putri Valenci',
+            // style: TextStyle(
+            //   color: Colors.white,
+            //   fontSize: 30.0,
+            //   ),
+            // ),
           ),
-          ExpansionTile(title: const Text('Materi'),
-          children: [
-            ListTile(
-              title: const Text('Materi 1'),
-              onTap: (){Navigator.pop(context);},
-            ),
-            ListTile(
-              title: const Text('Materi 2'),
-              onTap: (){Navigator.pop(context);},
-            ),
-            ListTile(
-              title: const Text('Materi 3'),
-              onTap: (){Navigator.pop(context);},
-            ),
-            ListTile(
-              title: const Text('Materi 4'),
-              onTap: (){Navigator.pop(context);},
-            ),
-            ListTile(
-              title: const Text('Materi 5'),
-              onTap: (){Navigator.pop(context);},
-            ),
-          ],
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Sierra'),
+            subtitle: Text('XII RPL 2'),
+            trailing: Icon(Icons.chevron_right),
           ),
-          ExpansionTile(title: const Text('Tugas'),
-          children: [
-            ListTile(
-              title: const Text('Tugas 1'),
-              onTap: (){Navigator.pop(context);},
-            ),
-            ListTile(
-              title: const Text('Tugas 2'),
-              onTap: (){Navigator.pop(context);},
-            ),
-            ListTile(
-              title: const Text('Tugas 3'),
-              onTap: (){Navigator.pop(context);},
-            ),
-            ListTile(
-              title: const Text('Tugas 4'),
-              onTap: (){Navigator.pop(context);},
-            ),
-            ListTile(
-              title: const Text('Tugas 5'),
-              onTap: (){Navigator.pop(context);},
-            ),
-          ],
-          ),
+          // ExpansionTile(title: const Text('Materi'),
+          // children: [
+          //   ListTile(
+          //     title: const Text('Materi 1'),
+          //     onTap: (){Navigator.pop(context);},
+          //   ),
+          //   ListTile(
+          //     title: const Text('Materi 2'),
+          //     onTap: (){Navigator.pop(context);},
+          //   ),
+          //   ListTile(
+          //     title: const Text('Materi 3'),
+          //     onTap: (){Navigator.pop(context);},
+          //   ),
+          //   ListTile(
+          //     title: const Text('Materi 4'),
+          //     onTap: (){Navigator.pop(context);},
+          //   ),
+          //   ListTile(
+          //     title: const Text('Materi 5'),
+          //     onTap: (){Navigator.pop(context);},
+          //   ),
+          // ],
+          // ),
+          // ExpansionTile(title: const Text('Tugas'),
+          // children: [
+          //   ListTile(
+          //     title: const Text('Tugas 1'),
+          //     onTap: (){Navigator.pop(context);},
+          //   ),
+          //   ListTile(
+          //     title: const Text('Tugas 2'),
+          //     onTap: (){Navigator.pop(context);},
+          //   ),
+          //   ListTile(
+          //     title: const Text('Tugas 3'),
+          //     onTap: (){Navigator.pop(context);},
+          //   ),
+          //   ListTile(
+          //     title: const Text('Tugas 4'),
+          //     onTap: (){Navigator.pop(context);},
+          //   ),
+          //   ListTile(
+          //     title: const Text('Tugas 5'),
+          //     onTap: (){Navigator.pop(context);},
+          //   ),
+          // ],
+          // ),
         ],
       ),
     );
